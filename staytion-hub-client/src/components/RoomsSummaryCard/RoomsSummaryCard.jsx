f8a48f6cc1770782ed3cd55339099370d9a7e751
Viswa Harini Vidyashankar <viswavidya16@gmail.com>
2024-10-26 23:53:23 -0400

    add pie chart

--- staytion-hub-client/src/components/RoomsSummaryCard/RoomsSummaryCard.jsx
+++ staytion-hub-client/src/components/RoomsSummaryCard/RoomsSummaryCard.jsx
@@ -1,35 +1,9 @@
 import './RoomsSummaryCard.scss';
-import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
+
 import Card from '../Card/Card';
 
-function RoomsSummaryCard () {
-
-    
-    const [roomsList, setRoomsList] = useState([]);
-    const [isError, setIsError] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const url = import.meta.env.VITE_API_URL;
-
-// get the list of all rooms to display list
-    const getList = useCallback(async () => {
-        setIsLoading(true);
-        setIsError(false);
-
-        try {
-          const { data } = await axios.get(`${url}/api/rooms`);
-          console.log(data);
-          setRoomsList(data);
-          setIsLoading(false);
-        } catch (error) {
-          console.error(`Could not fetch rooms list ${error}`);
-          setIsError(true);
-        }
-      }, []);
-
-      useEffect(() => {
-        getList();
-      }, [getList]);
+function RoomsSummaryCard ({roomsList}) {
+  console.log(roomsList);
 
       const occupiedRooms = roomsList.filter((room) => { return room.status === 'Occupied'})
       console.log(occupiedRooms);
--- staytion-hub-client/src/pages/DashboardPage/DashboardPage.jsx
+++ staytion-hub-client/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,10 +1,40 @@
 import Layout from "../../components/Layout/Layout";
 import ReservationsSummaryCard from "../../components/ReservationsSummaryCard/ReservationsSummaryCard";
 import RoomsSummaryCard from "../../components/RoomsSummaryCard/RoomsSummaryCard";
+import RoomsPieChart from "../../components/RoomsPieChart/RoomsPieChart";
 import './DashboardPage.scss';
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
 
 function DashboardPage () {
     const block='dashboard';
+
+    const [roomsList, setRoomsList] = useState([]);
+    const [isError, setIsError] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const url = import.meta.env.VITE_API_URL;
+
+// get the list of all rooms to display list
+    const getList = useCallback(async () => {
+        setIsLoading(true);
+        setIsError(false);
+
+        try {
+          const { data } = await axios.get(`${url}/api/rooms`);
+          console.log(data);
+          setRoomsList(data);
+          setIsLoading(false);
+        } catch (error) {
+          console.error(`Could not fetch rooms list ${error}`);
+          setIsError(true);
+        }
+      }, []);
+
+      useEffect(() => {
+        getList();
+      }, [getList]);
+
+
     return (
         <Layout className={block}>
              <nav className={`${block}__navigation`}>
@@ -12,9 +42,11 @@ function DashboardPage () {
             </nav>
             <section className={`${block}__summary`}>
                 <ReservationsSummaryCard />
-                <RoomsSummaryCard />
+                <RoomsSummaryCard roomsList={roomsList} />
             </section>
-
+            <section className={`${block}__chart`}>
+              <RoomsPieChart roomsList={roomsList} />
+            </section>
         </Layout>
     );
 }
